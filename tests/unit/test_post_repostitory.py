052b12585d86252f2d05b58d1d24e14294a45ba0
matt.dawson@artlogic.net <matt.dawson@artlogic.net>
2024-12-03 10:23:09 +0000

    Parameterise test

--- tests/unit/test_post_repostitory.py
+++ tests/unit/test_post_repostitory.py
@@ -4,6 +4,8 @@ from social_network_kata.post_repository import PostRepository
 from social_network_kata.post import Post
 from datetime import datetime
 
+import pytest
+
 
 class TestPostRepository:
     def test_can_create_post(self):
@@ -17,13 +19,12 @@ class TestPostRepository:
 
         assert post_repo.get_posts() == [Post(clock.get_date.return_value, name, message)]
 
-    def test_can_get_posts_by_user(self):
+    @pytest.mark.parametrize("input, output", [("Bob", "Good"), ("McBob", "bye"), ("David", "I am a human")])
+    def test_can_get_posts_by_user(self, input, output):
         clock = Mock(Clock)
         clock.get_date.return_value = datetime.now()
         post_repo = PostRepository(clock)
-        names = ["Bob", "McBob"]
-        messages = ["Good","bye"]
         
-        post_repo.create_post(names[0],messages[0])
+        post_repo.create_post(input, output)
 
-        assert post_repo.get_posts_by_user(names[0]) == [Post(clock.get_date.return_value, names[0], messages[0])]
+        assert post_repo.get_posts_by_user(input) == [Post(clock.get_date.return_value, input, output)]
